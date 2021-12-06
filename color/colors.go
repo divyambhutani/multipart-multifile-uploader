a4db82168b74492fe67155fa43734c9652e4147b
divyam_bhutani <divyam@convosight.com>
2021-12-06 09:24:34 +0530

    added color submodule to provide colors for log info and error

---
+++ color/colors.go
@@ -0,0 +1,12 @@
+package color
+
+const (
+	ORANGE      = "\033[1;34m%s\033[0m"
+	LIGHTYELLOW = "\033[1;33m%s\033[0m"
+	RED         = "\033[1;31m%s\033[0m"
+	GREEN       = "\033[1;32m%s\033[0m"
+	LIGHTBLUE   = "\033[1;36m%s\033[0m"
+	GREY        = "\033[1;30m%s\033[0m"
+	PURPLE      = "\033[1;35m%s\033[0m"
+	UNDERLINE   = "\033[1;21m%s\033[0m"
+)
